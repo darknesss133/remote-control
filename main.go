61b023d9e1ae7d44604392022da9a4b5c1a0b920
darknesss133 <darknesss133@mail.ru>
2021-02-18 18:13:46 +0400

    Авторизация Google

--- main.go
+++ main.go
@@ -9,7 +9,10 @@ import (
 )
 
 var (
-	addr = flag.String("addr", ":8080", "TCP address to listen to")
+	addr      = flag.String("addr", ":8080", "TCP address to listen to")
+	url       = "https://accounts.google.com/o/oauth2/auth"
+	client_id = "581640295716-h3aqeb6receolg99bq82jmtumcsq1l85.apps.googleusercontent.com"
+	scope     = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
 )
 
 func main() {
@@ -23,11 +26,8 @@ func main() {
 func requestHandler(ctx *fasthttp.RequestCtx) {
 	fmt.Fprintf(ctx, "Hello, world!\n\n")
 	fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
-	fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
-	fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
 	fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
 	fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
-	fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
 	fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
 	fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
 	fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
