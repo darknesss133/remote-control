1a809be7aebfddac251a0e09e855f5f937f5690d
darknesss133 <darknesss133@mail.ru>
2021-02-22 12:42:10 +0400

    Гугл авторизация с извлечением данных

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -8,11 +9,41 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
+//структура для хранения данных о пользователе
+type user struct {
+	id             string
+	email          string
+	verified_email bool
+	name           string
+	given_name     string
+	family_name    string
+	picture        string
+	locale         string
+}
+
+//структура для хранения данных о токене пользователя
+type token struct {
+	access_token      string
+	token_type        string
+	expires_in        float64
+	id_token          string
+	error_description string
+}
+
 var (
-	addr      = flag.String("addr", ":8080", "TCP address to listen to")
-	url       = "https://accounts.google.com/o/oauth2/auth"
-	client_id = "581640295716-h3aqeb6receolg99bq82jmtumcsq1l85.apps.googleusercontent.com"
-	scope     = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
+	userToken    token
+	userInfo     user
+	a            []byte
+	queryArgs    *fasthttp.Args
+	authCode     string
+	addr         = flag.String("addr", ":8080", "TCP address to listen to")
+	authUrl      = "https://accounts.google.com/o/oauth2/auth"
+	accessUrl    = "https://www.googleapis.com/oauth2/v1/userinfo"
+	tokenUrl     = "https://accounts.google.com/o/oauth2/token"
+	clientSecret = "rERfKmVC2J0ege8bw6YAZOnU"
+	clientId     = "581640295716-h3aqeb6receolg99bq82jmtumcsq1l85.apps.googleusercontent.com"
+	grantType    = "authorization_code"
+	scope        = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
 )
 
 func main() {
@@ -24,13 +55,23 @@ func main() {
 }
 
 func requestHandler(ctx *fasthttp.RequestCtx) {
-	fmt.Fprintf(ctx, "Hello, world!\n\n")
-	fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
-	fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
-	fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
-	fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
-	fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
-	fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
-	fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
-	ctx.SetContentType("text/plain; charset=utf8")
+	queryArgs = ctx.QueryArgs()
+	authCode = string(queryArgs.Peek("code"))
+	queryArgs.Reset()
+	queryArgs.Add("client_id", clientId)
+	queryArgs.Add("client_secret", clientSecret)
+	queryArgs.Add("redirect_uri", "http://localhost"+*addr)
+	queryArgs.Add("grant_type", grantType)
+	queryArgs.Add("code", authCode)
+	_, a, _ = fasthttp.Post(a, tokenUrl, queryArgs)
+	var f interface{}
+	json.Unmarshal(a, &f)
+	m := f.(map[string]interface{})
+	_, a, _ = fasthttp.Get(a, accessUrl+"?access_token="+m["access_token"].(string))
+	json.Unmarshal(a, &f)
+	n := f.(map[string]interface{})
+	fmt.Fprintf(ctx, "Hello %s!\n\n", n["name"])
+	for key, value := range n {
+		fmt.Fprintf(ctx, "%q : %q\n", key, value)
+	}
 }
